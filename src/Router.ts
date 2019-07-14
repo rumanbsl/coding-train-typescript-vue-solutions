a190f86070b732827fd0fca9df195c9c20002820
Majedul Hoque <rumanbsl@gmail.com>
2019-07-14 18:22:54 +0300

    autonomouse WIP

--- src/Router.ts
+++ src/Router.ts
@@ -2,11 +2,13 @@ import VueRouter from "vue-router";
 
 import exercise from "./Lessons/exercise";
 import force from "@/Lessons/force";
+import autonomousAgent from "@/Lessons/autonomous-agent";
 
 const routes = [
   // module 1
   { path: "/", redirect: "/exercise" },
   { path: "/force", component: force },
+  { path: "/autonomous", component: autonomousAgent },
   { path: "/exercise", component: exercise },
 ];
 
